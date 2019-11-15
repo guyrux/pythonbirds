e88b3723bcf9d305c6c847300df99cf708f48675
GustavoSuto <gustavo.suto@usp.br>
2019-11-15 19:01:17 -0300

    created instance attributes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome="Gustavo", idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f"Olá, {id(self)}"
 
@@ -6,3 +10,6 @@ if __name__ == '__main__':
     p = Pessoa()
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p = Pessoa("Suto")
+    print(p.nome)
