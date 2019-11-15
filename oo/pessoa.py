85b14ba1be5eba6e22f5b226f84aab33d6cf06c3
GustavoSuto <gustavo.suto@usp.br>
2019-11-15 19:22:31 -0300

    created complex instance attributes

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,16 @@
 class Pessoa:
-    def __init__(self, nome="Gustavo", idade=35):
+    def __init__(self, *filhos, nome="Gustavo", idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f"Olá, {id(self)}"
+        return f"Olá, {self.nome}!"
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p = Pessoa("Suto")
-    print(p.nome)
+    filho01 = Pessoa(nome="Gu", idade=34)
+    filho02 = Pessoa(nome="Gui", idade=32)
+    pai = Pessoa(filho01, filho02, nome="José", idade=64)
+    print(pai.cumprimentar())
+    print(f"{pai.nome} tem {pai.idade} anos e {len(pai.filhos)} filhos.")
+
