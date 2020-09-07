ea88ad96dbd96bc9090c2bc5052fcf2bc3550d4a
Gustavo Suto <guyrux@gmail.com>
2020-09-07 15:05:08 -0300

    [feat] Criado atributo dinâmico Pessoa.sobrenome.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,9 +9,13 @@ class Pessoa:
 
 if __name__ == "__main__":
     gustavo = Pessoa(nome="Gustavo")
-    jose = Pessoa(gustavo, nome="José")
+    jose = Pessoa(gustavo, nome="José", idade=66)
     print(jose.nome)
     print(id(jose))
     print(jose.cumprimentar())
     for filho in jose.filhos:
         print(f"Um dos filhos de {jose.nome} chama {filho.nome}.")
+
+    gustavo.sobrenome = "Suto"  # Criando um atributo dinâmico
+    print(gustavo.__dict__)     # Acessando todos os atributos de um objeto, mesmo os atributos dinâmicos.
+    print(jose.__dict__)
