f6d93f505343aea48e9adc0c5314f3ec5e06342a
Gustavo Suto <guyrux@gmail.com>
2020-09-07 12:35:31 -0300

    [feat] Criado atributo nome da classe Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+    
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
 if __name__ == "__main__":
-    p = Pessoa()
+    p = Pessoa("Gustavo")
+    print(p.nome)
     print(id(p))
     print(p.cumprimentar())
+    p.nome = "Suto"
+    print(p.nome)
