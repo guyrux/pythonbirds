97b6157d49fd0419f9dba59a223672f8f66c852b
Gustavo Suto <guyrux@gmail.com>
2020-09-07 14:56:07 -0300

    [feat] Criado atributo complexo.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,17 @@
 class Pessoa:
-    def __init__(self, nome=None):
+    def __init__(self, *filhos,nome=None, idade=35):
         self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
     
     def cumprimentar(self):
         return f"Olá {id(self)}"
 
 if __name__ == "__main__":
-    p = Pessoa("Gustavo")
-    print(p.nome)
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = "Suto"
-    print(p.nome)
+    gustavo = Pessoa(nome="Gustavo")
+    jose = Pessoa(gustavo, nome="José")
+    print(jose.nome)
+    print(id(jose))
+    print(jose.cumprimentar())
+    for filho in jose.filhos:
+        print(f"Um dos filhos de {jose.nome} chama {filho.nome}.")
